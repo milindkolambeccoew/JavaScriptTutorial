69373a6ecbfc4da177521868421c5c97e5ca9a22
milindkolambeccoew <milind.kolambe@cumminscollege.in>
2024-06-06 23:06:47 +0530

    DOM1

--- 01_Script.js
+++ 01_Script.js
@@ -1,88 +1,106 @@
-/*
-console.log('Hello') //print anything on the console window
-a=5    // variable 
-a=null   // no value in it
-a=undefined  // not defined yet
+
+// console.log('Hello') //print anything on the console window
+// a=5    // variable 
+// a=null   // no value in it
+// a=undefined  // not defined yet
 
 //Array
-let marks = [10,15,20,25,30];
-for (let m of marks) {
-    console.log(m);
-}
+// let marks = [10,15,20,25,30];
+// for (let m of marks) {
+//     console.log(m);
+// }
 
 //Array
-for (let m in marks) {
-    console.log(marks[m]);
-}
+// for (let m in marks) {
+//     console.log(marks[m]);
+// }
 
 //Array
-console.log("Marks converted in strings are:", marks.toString());
+// console.log("Marks converted in strings are:", marks.toString());
 
 
 //Functions without parameters and return value
-function message{
-    console.log("This function has not parameters and no return value");
-}
+// function message{
+//     console.log("This function has not parameters and no return value");
+// }
 
-message();
+// message();
 
 //function with parameters
-function multiply(a,b){
-console.log("multiplication=",a*b);
-}
+// function multiply(a,b){
+// console.log("multiplication=",a*b);
+// }
 
-multiply(10,20);
+// multiply(10,20);
 
 //function with paramters and return value.
-function multiply2(a,b){
-    return a*b;
-}
+// function multiply2(a,b){
+//     return a*b;
+// }
 
-c=multiply2(20,5);
+// c=multiply2(20,5);
 
 //Arrow function are anonymous (or lambda) functions with a const variable assigned to each
-const mult=(a,b)=>{
-    console.log(a*b);
-}  // we could skip pair of braces for single line body.
+// const mult=(a,b)=>{
+//     console.log(a*b);
+// }  // we could skip pair of braces for single line body.
 
-mult(10,90);
+// mult(10,90);
 
 //ForEach method 
-let marks=[10,20,30,40,50];  //forEach is a method used for array only.
-//methods are functions used for specific object only. e.g. for strings we have 
-//str.Toupper() method.
-marks.forEach((val)=>{   //we can use 'idx' and 'arr' as well like val.
-console.log(val);
-})
+// let marks=[10,20,30,40,50];  //forEach is a method used for array only.
+// //methods are functions used for specific object only. e.g. for strings we have 
+// //str.Toupper() method.
+// marks.forEach((val)=>{   //we can use 'idx' and 'arr' as well like val.
+// console.log(val);
+// })
 
 //map method (similliar to ForEach but can create new array modified with operation)
-let mark2=[10,20,30,40,50];
-let marksSq = mark2.map((val)=>{
-    return (val*val);
-});
+// let mark2=[10,20,30,40,50];
+// let marksSq = mark2.map((val)=>{
+//     return (val*val);
+// });
 
-console.log(marksSq);
+// console.log(marksSq);
 
 //filter method
-let marks3=[11,20,31,40,51];
-let marksEven=marks3.filter((val)=>{
-    return (val%2===0);
-})
+// let marks3=[11,20,31,40,51];
+// let marksEven=marks3.filter((val)=>{
+//     return (val%2===0);
+// })
 
-console.log(marksEven);
+// console.log(marksEven);
 
 //reduce method (return 'one' value only. It could be 'sum', 'largestno' etc)
-let marks4 = [10,20,30,40,50];
-let largest = marks4.reduce((prev,curr)=>{
-    return (prev>curr?prev:curr);
-})
+// let marks4 = [10,20,30,40,50];
+// let largest = marks4.reduce((prev,curr)=>{
+//     return (prev>curr?prev:curr);
+// })
 
-console.log("Largest no is:",largest);
+// console.log("Largest no is:",largest);
 
-let sum=marks4.reduce((prev,curr)=>{
-    return prev+curr;
-})
+// let sum=marks4.reduce((prev,curr)=>{
+//     return prev+curr;
+// })
 
-console.log("Sum of array is:",sum);
+// console.log("Sum of array is:",sum);
 
-*/
+
+//************************************************************
+//DOM Strts here....let btn=document.querySelector("#mybtn")
+
+// let div=document.querySelector("#div1")
+// console.log(div)
+// console.log(div.children) //firstchild, lastchild etc.
+
+// let btn=document.querySelectorAll("button")
+// console.log(btn[0].innerText)
+// // We can get and set innerText and innerHtml as well.
+
+// let btn1=document.querySelector("button");
+// console.log(btn1.getAttribute("id"));
+
+// let btn1=document.querySelector("button");
+// btn1.setAttribute("id","btn11");
+// console.log(btn1.getAttribute("id"));
+
